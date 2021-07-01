93196b5e64af20be0f1ffcd230a82a8a94033dfd
Aldebarancloud <45065026+Aldebarancloud@users.noreply.github.com>
2021-07-01 18:21:46 +0200

    update

--- Module-4-Golden-Image-Creation/VM-WindowsServer-with-graphic-card/Nvdia-VM/Golden-Image-With-Teams-OneDrive-plus-Software/Nested/TeamsFullInstallationforWVDOneDriveandGraphicCardConfigplusSoftware.ps1
+++ Module-4-Golden-Image-Creation/VM-WindowsServer-with-graphic-card/Nvdia-VM/Golden-Image-With-Teams-OneDrive-plus-Software/Nested/TeamsFullInstallationforWVDOneDriveandGraphicCardConfigplusSoftware.ps1
@@ -1,5 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+#Software
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install adobereader -y
 choco install googlechrome -y
@@ -17,6 +18,13 @@ choco install winrar -y
 #regedit teams for wvd
 reg add "HKLM\SOFTWARE\Microsoft\Teams" /v IsWVDEnvironment /t REG_DWORD /d 1 /f
 
+#regedit for Graphic Card for WVD
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v bEnumerateHWBeforeSW  /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v AVC444ModePreferred  /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v AVCHardwareEncodePreferred  /t REG_DWORD /d 1 /f
+
+gpupdate.exe /force
+
 #Variables
 $CSource = "https://aka.ms/vs/16/release/vc_redist.x64.exe"
 $RDWRedirectorSource = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RE4AQBt"
@@ -24,6 +32,10 @@ $TeamsSource = "https://statics.teams.cdn.office.net/production-windows-x64/1.4.
 $Clocation = "C:\temp\vc_redist.x64.exe"
 $RDWRedirectionLocation = "C:\temp\MsRdcWebRTCSvc_HostSetup_1.0.2006.11001_x64.msi"
 $TeamsLocation = "C:\temp\Teams_windows_x64.msi"
+$FurMarkSource = "https://github.com/Aldebarancloud/WVDCourse/blob/main/Lab4.1ConfiguretheGPU/VMConfiguration/FurMark_1.25.0.0_Setup.exe?raw=true"
+$locationFurMark = "C:\temp\FuRMark\FurMark_1.25.0.0_Setup.exe"
+$OneDriveSource = "https://go.microsoft.com/fwlink/?linkid=844652"
+$locationOneDrivedownload = "C:\temp\OneDriveSetup.exe"
 
 #log store
 [string]$temPAth = 'C:\temp\'
@@ -49,6 +61,12 @@ Start-Sleep -s 5
 #Download Teams 
 invoke-WebRequest -Uri $TeamsSource -OutFile $TeamsLocation
 Start-Sleep -s 5
+#Download FurMark 
+Start-BitsTransfer -Source $FurMarkSource -Destination $locationFurMark
+Start-Sleep -s 5
+#Download OneDrive Per-Machine
+Invoke-WebRequest -Uri $OneDriveSource -OutFile $locationOneDrivedownload
+Start-Sleep -s 5
 
 #Install C++ runtime
 Start-Process -FilePath $Clocation -ArgumentList '/q', '/norestart'
@@ -56,6 +74,12 @@ Start-Sleep -s 10
 #Install MSRDCWEBTRCSVC
 msiexec /i $RDWRedirectionLocation /q /n
 Start-Sleep -s 10
-# Install Teams
+#Install Teams
 msiexec /i $TeamsLocation /l*v teamsinstall.txt ALLUSER=1 ALLUSERS=1 /q
+Start-Sleep -s 10
+#Install FurMark 
+Start-Process -FilePath "$locationFurMark" /quiet
+Start-Sleep -s 10
+#Install OneDrive
+C:\OneDriveSetup.exe /allusers
 Start-Sleep -s 10
